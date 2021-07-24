ca36ae691100bdb2a1020c1153639d6dfb729901
Dorlac <jacobdorlac@gmail.com>
2021-07-24 09:46:42 -0700

    Set up prototype landing page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+ReactDOM.render(
+    <html>
+    <body>
+    <h1>Bright Side University</h1>
+    <h2>Brighten your future with a carrer in IT</h2>
+    <p>Our mission is to provide Devops, Software, and Machine Learning Engineering
+     educational resources to those with the aptitude and desire to improve their 
+     quality of life with a career in IT.</p>
+    <button>Sign up for Bright Side University</button>
+    </body>
+    </html>,
+    document.getElementById('root')
+  );
