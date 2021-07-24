7c548ab93c75f8bf8a87ab893748dc97c9b15e7a
Dorlac <jacobdorlac@gmail.com>
2021-07-24 12:08:42 -0700

    Update form

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import './index.css';
 ReactDOM.render(
     <html>
     <body>
-    <h1>Bright Side University</h1>
-    <h2>Brighten your future with a carrer in IT</h2>
+    <h1>Brighten your future with marketable skills from
+    Bright Side University</h1>
+    <p>Bright Side University is still in development.
+     Enter your email below to be notified when development is complete.</p>
     <p>Our mission is to provide Devops, Software, and Machine Learning Engineering
      educational resources to those with the aptitude and desire to improve their 
      quality of life with a career in IT.</p>
-    <button>Sign up for Bright Side University</button>
+    <form>
+      <p>Get notified when the development is complete</p>
+      <input id="email" type="email" placeholder="Email address"/>
+      <button>Sign up for updates</button>
+    </form>
     </body>
     </html>,
     document.getElementById('root')
