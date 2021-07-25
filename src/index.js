f5edb9885d41e53d1ab8f198ab257fc068801eae
Dorlac <jacobdorlac@gmail.com>
2021-07-25 13:24:07 -0700

    Finish basic landing page

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import logo from './images/logo.png'
 
 ReactDOM.render(
-    <html>
-    <body>
-    <h1>Brighten your future with marketable skills from
-    Bright Side University</h1>
-    <p>Bright Side University is still in development.
-     Enter your email below to be notified when development is complete.</p>
-    <p>Our mission is to provide Devops, Software, and Machine Learning Engineering
-     educational resources to those with the aptitude and desire to improve their 
-     quality of life with a career in IT.</p>
-    <form>
-      <p>Get notified when the development is complete</p>
-      <input id="email" type="email" placeholder="Email address"/>
-      <button>Sign up for updates</button>
+  <body>
+  <nav>
+    <a href="https://brightsideuniversity.com">
+      <img align="left" src={logo} height="48px" width="48px" />
+    </a>
+  </nav>
+  <br />
+  <main>
+    <h1 align="center">Bright Side School of Engineering</h1>
+    <form action="#">
+      <p>
+        This website is still in developement.
+        Enter your email to receive progress updates.
+      </p>
+      <input id="email" type="email" placeholder="Email address" required/>
+      <button type="submit">Sign up for updates</button>
     </form>
-    </body>
-    </html>,
+    </main>
+    <footer>
+    <a 
+      href="https://linkedin.com/company/bright-side-university"
+      target="_blank">
+      Follow us on LinkedIn
+    </a>
+    {/* <img 
+      src="https://media.istockphoto.com/photos/beautiful-blazing-sunset-landscape-at-over-the-meadow-and-orange-sky-picture-id854130274?k=6&m=854130274&s=612x612&w=0&h=p21BlqMlntinZp5jL3Am40vvMRZ2xZHGsIIoviobkGw="
+    /> */}
+    </footer>
+    </body>,
     document.getElementById('root')
   );
